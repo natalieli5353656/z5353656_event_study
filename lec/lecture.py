bd1ccaeee08b938e0f27ec64d728aee56dfe3bae
z5353656_lecture <lingyunli99@gmail.com>
2023-11-16 22:24:27 +1100

    initial commit

---
+++ lec/lecture.py
@@ -0,0 +1,22 @@
+""" z5353656_project1.py
+
+"""
+import json
+import os
+
+import toolkit_config as cfg
+
+# ----------------------------------------------------------------------------
+# Location of files and folders
+# Instructions:
+#   - Replace the '<COMPLETE THIS PART>' strings with the appropriate
+#   expressions.
+# IMPORTANT:
+#   - Use the appropriate method from the `os` module to combine paths
+#   - Do **NOT** include full paths like "C:\\User...". You **MUST* combine
+#     paths using methods from the `os` module
+#   - See the assessment description for more information
+# ----------------------------------------------------------------------------
+ROOTDIR = os.path.join(cfg.PRJDIR, 'project1')
+DATDIR = os.path.join(ROOTDIR, 'data')
+TICPATH = os.path.join(ROOTDIR, 'TICKERS.txt')
